da71f664ab7ee4f5e16c39b00318fe07d5458b9b
Tzuzul Rosas <tzuzulrosas@gmail.com>
2025-03-11 14:03:47 -0600

    Linked list removeLast method

--- 09 - Listas/index.js
+++ 09 - Listas/index.js
@@ -32,7 +32,51 @@ class LinkedList{
         current.next = newNode
     }
 
-    // Reto: Remover el ultimo elemento de la lista
+    // Reto: Remover el ultimo elemento de la lista y retornar el ultimo elemento
+    removeLast(){
+        // 1. Verificamos que no esta vacia
+        if(!this.head){
+            return
+        }
+
+        // 1.5 Verificamos cuando solo hay un elemento
+        if(!this.head.next){
+            this.head = null
+            return
+        }
+
+        // 2. Definimos current
+        let current = this.head
+        
+        // 3. Recorremos lista con while
+        while(current.next.next){
+            current = current.next 
+        }
+
+        let last = current.next
+        current.next = null
+
+        return last
+
+        /*
+        //Solucion standar
+/       / 2. Definimos current
+        let current = this.head
+        let previous
+        
+        // 3. Recorremos lista con while
+        while(current.next){
+            previous = current
+            current = current.next 
+        }
+
+        previous.next = null
+
+        return last
+
+        */
+
+    }
 
     print(){
         let current = this.head
@@ -48,6 +92,8 @@ const linkedList = new LinkedList()
 linkedList.append(5)
 linkedList.append(15)
 linkedList.append(25)
+let removed = linkedList.removeLast()
+console.log(removed)
 
 linkedList.print()
 
@@ -57,3 +103,5 @@ array.push(15)
 array.push(25)
 
 console.log(array)
+
+// Reto: Implementar el metodo append para una lista enlazada doble.
